66e164d16ca27b966324813232a3c598f6dde0c3
Nikhiy <your_email@gmail.com>
2026-02-21 17:39:29 +0530

    added frontend

---
+++ frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+export const authService='http://localhost:5000'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <GoogleOAuthProvider clientId="638233768044-99f7blj37iavuob63r7fvo6tjhc8b0ut.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>,
+)
